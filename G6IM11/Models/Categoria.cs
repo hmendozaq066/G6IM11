c71e1bd45b9d8bb64bdbcf3f9767d1a6969f3e81
hmendozaq <hmendoza@ipn.mx>
2022-03-08 13:30:07 -0600

    08 0130

--- G6IM11/Models/Categoria.cs
+++ G6IM11/Models/Categoria.cs
@@ -1,7 +1,20 @@
-﻿namespace G6IM11.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace G6IM11.Models
 {
+    //1.-Agregar using
     public class Categoria
     {
-        //Hola
+        [Key]
+        [Display(Name = "Categoría")]
+        public int Categoria_id { get; set; }
+        [Required]
+        [MinLength(10)]
+        [MaxLength(60)]
+        [Display(Name = "Descripción de la categoría")]
+        public string Descripcion { get; set; }
     }
 }
---
+++ G6IM11/Models/Rol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace G6IM11.Models
+{
+    public class Rol
+    {
+        [Key]
+        public int Rol_id { get; set; }
+        //ErrorMessage nos permite modificar el mensaje de error en la validación del modelo
+        [Required(ErrorMessage = "El nombre del rol es obligatorio")]
+        [MinLength(10, ErrorMessage = "El nombre del rol debe tener mínimo 10 letras")]
+        [MaxLength(60, ErrorMessage = "El nombre del rol debe tener máximo 60 letras")]
+        public string Descripcion { get; set; }
+    }
+}
